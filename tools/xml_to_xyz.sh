58a31334324152532a4d05107c68bae94a5763a5
Simon König <simon@Simons-MacBook-Pro.local>
2021-02-17 12:11:23 +0100

    simon, added xml_to_xyz.sh script, but not quite finished yet simon, added xml_to_xyz.sh script, but not quite finished yet

--- tools/xml_to_xyz.sh
+++ tools/xml_to_xyz.sh
@@ -1,40 +1,112 @@
-file=$1
+# Define infile and outfile
+infile=$1
 outfile=$2
 
-# Get Number of atoms in simulation (shoulb be $natom for $file)
-natom=`awk '{if($1 == "<atoms>") print $2;}' $file `
-nsteps=`grep forces $file | wc -l`
+# Get Number of atoms in simulation (should be $natom for $infile)
+natom=`awk '{if($1 == "<atoms>") print $2;}' $infile `
 
-echo "Read number of MD steps from " $file " file. nsteps=" $nsteps
-echo "Read number of atoms from " $file " file. natoms=" $natom
+# Get Number of time steps (should be $nsteps for $infile)
+nsteps=`grep forces $infile | wc -l`
 
-for i in `seq 1 1 $nsteps`
+# Get starting line in .xyz-file. e.g. where the simulation starts and the nsteps begin
+startline=`grep -m1 -n velocities vasprun.xml | awk '{print substr($1,1,length($1)-1)}'`
+
+# Get all lines after line with 'positions' after startline
+#positions_1=`tail -n +$startline $infile | grep -A  $natom 'positions' | awk '{print $2,$3,$4}'`
+
+# Get all lines after line with 'forces' after startline
+#forces_1=`tail -n +$startline $infile | grep -A  $natom 'forces' | awk '{print $2,$3,$4}'`
+
+# Get the 9 lattice constants
+lattice=`grep  -A 3 -m 2 \"basis\" $infile | awk '{print$2,$3,$4}' | tail -n+2 | head -n+3`
+
+# Get the atom - element e.g. H (Hydrogen)
+atom_type=`grep -A $natom '>atomtype' $infile | awk -F':' '{split($1,subfield,"");print subfield[12] }' | tail -n +3`
+
+# Get all lines after line with 'positions', just the coordinates without text
+# Don´t know how to implement startline
+positions=`tail -n $startline vasprun.xml | grep  -A  $natom 'positions' $infile | sed -e '/positions/,+d' | awk '{print $2,$3,$4}'`
+
+# Get all lines after line with 'forces', just the coordinates without text
+# Don´t know how to implement startline
+forces=`tail -n $startline vasprun.xml | grep  -A  $natom 'forces' $infile | sed -e '/forces/,+d' | awk '{print $2,$3,$4}'`
+
+
+
+
+echo "Read number of MD steps from " $infile " infile. nsteps = " $nsteps
+echo "Read number of atoms from " $infile " infile. natoms = " $natom
+echo "Read line number at which the simulationstarts from " $infile "infile = " $startline
+echo "Read lattice constants from " $infile "infile." "lattice constants = " $lattice
+
+# Transfer positions, forces and atom_types to arrays for better indexing possibilities
+pos_array=( $positions )
+for_array=( $forces )
+atom_type_array=( $atom_type )
+
+
+
+# Write the output to outfile
+# Outfile doesn´t resemble the .xml file. Arrays contain correct values(except
+# pos_array is too long by one step).
+
+for i in `seq 0 1 $nsteps`
 do
 
-echo $i > $outfile
-
-echo "appended output" >> $outfile
-echo $nsteps " " $natoms
+    echo $natom >> $outfile
+    echo "Lattice=$lattice Properties=species:S:1:pos:R:3:
+    forces:R:3:energies:R:1 Energie=dummy_energy pbc=\"T T T\"" >> $outfile
+    for j in `seq 0 1 ${natom-1}`
+    do
+        echo ${atom_type_array[$j]} ${pos_array[${i}*${natom}+${j}*3]} ${pos_array[${i}*${natom}+${j}*3+1]} \
+        ${pos_array[${i}*${natom}+${j}*3+2]} ${for_array[${i}*${natom}+${j}*3]} ${for_array[${i}*${natom}+${j}*3+1]} \
+        ${for_array[${i}*${natom}+${j}*3+2]} >> $outfile
+    done
 
 done
 
-# Get all lines after line with 'positions'
-#grep -A $natom 'positions' $file | awk '{print $2,$3,$4}'
+# Write the output to outfile, just a template and not for further use
+# for i in `seq 1 1 $nsteps`
+# do
+#
+#    echo $natom >> $outfile
+#    echo "Lattice=$lattice Properties=species:S:1:pos:R:3:
+#    forces:R:3:energies:R:1 Energie=dummy_energy pbc=\"T T T\"" >> $outfile
+#
+#    #echo $positions $ forces>> $outfile
+#
+#    echo "appended output" >> $outfile
+#    #echo $nsteps " " $natoms
+#    #echo $positions $forces
+#
+# done
 
 
-# Get all lines after line with with 'forces'
-# grep -A $natom 'forces' $file | awk '{print $2,$3,$4}'
+# While loop to iterate over all positions, can only iterate over one argument
+# tail -n +$startline $infile | grep -A  $natom 'positions' | awk '{print $2,$3,$4}' | while read line
+# do
+# echo $line
+# done
+
+# not working, tried to loop over all positions
+# for (x,y,z) in $positions
+# do
+#    echo $x $y $z
+# done
+
+
+
 
 
 # Get the atom - element e.g. H (Hydrogen)
-# grep -A $natom '>atomtype' $file | awk -F':' '{split($1,subfield,"");print subfield[12] }' | tail -n +3
+# grep -A $natom '>atomtype' $infile | awk -F':' '{split($1,subfield,"");print subfield[12] }' | tail -n +3
 
 
-#TODO:
-# Get energies for each timestep
 
-# Get lattice constants
+
+# TODO:
+# Get energies for each timestep, uncertain which energies to use
 
 # Get energies of isolated atoms (e.g. e_0_Hydrogen)
 
-# concatenate all the needed values to a file format identical to .xyz (e.g. train.xyz)
+# concatenate all the needed values to a infile format identical to .xyz (e.g. train.xyz)
